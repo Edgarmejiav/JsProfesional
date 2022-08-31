4c7714877b6171682660492e9b30d8528dc6702c
edgarmejiav <edgarm_10@hotmail.com>
2022-08-31 16:00:26 -0500

    server worker

---
+++ website/assets/index.js
@@ -0,0 +1,25 @@
+import MediaPlayer from "./MediaPlayer.js";
+import AutoPlay from "./plugins/AutoPlay.js";
+import AutoPause from "./plugins/AutoPause.js";
+const video = document.querySelector("video");
+const player = new MediaPlayer({
+  el: video,
+  plugins: [new AutoPlay(), new AutoPause()],
+});
+const button = document.querySelector("button");
+button.onclick = () => player.togglePlay();
+
+const muteButton = document.querySelector("#muteButton");
+muteButton.onclick = () => {
+  if (player.media.muted) {
+    player.unmute();
+  } else {
+    player.mute();
+  }
+};
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/website/sw.js").catch((error) => {
+    console.log(error.message);
+  });
+}
---
+++ website/assets/plugins/AutoPause.js
@@ -0,0 +1,40 @@
+class AutoPause {
+  constructor() {
+    this.threshold = 0.25;
+
+    this.handleIntersection = this.handleIntersection.bind(this);
+  }
+  run(player) {
+    this.player = player;
+    const observer = new IntersectionObserver(this.handleIntersection, {
+      threshold: this.threshold,
+    });
+    observer.observe(this.player.media);
+
+    document.addEventListener("visibilitychange", () =>
+      this.handleVisibilityChange()
+    );
+  }
+  handleIntersection(entries) {
+    const entry = entries[0];
+
+    const isVisibile = entry.intersectionRatio >= this.threshold;
+
+    if (isVisibile) {
+      this.player.play();
+    } else {
+      this.player.pause();
+    }
+  }
+
+  handleVisibilityChange() {
+    const isVisible = document.visibilityState === "visible";
+    if (isVisible) {
+      this.player.play();
+    } else {
+      this.player.pause();
+    }
+  }
+}
+
+export default AutoPause;
